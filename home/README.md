780b791125231be285c7b435ba588c4625820d84
hairpin <vsmj012@163.com>
2017-10-29 15:46:29 +0800

    init

---
+++ home/README.md
@@ -0,0 +1,5 @@
+# home
+
+> A Vue.js project
+
+
---
+++ home/src/pages/nav.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        nav
+        <div class="used">常用</div>
+        <div class="delete">可以考虑删除导航</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'nav',
+    props: {},
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    },
+    created() { },
+    computed: {},
+    components: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
